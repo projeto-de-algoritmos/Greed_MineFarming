35dcabacbe1ba03710a6e19ed01f03754aceae38
Pedro Henrique <pedrodsm1819@gmail.com>
2023-10-29 15:34:48 -0300

    Adicionando novos tipos de vegetais

--- src/mine.jsx
+++ src/mine.jsx
@@ -1,8 +1,15 @@
 import "./mine.css"; 
 import Plant from "./plant";
-// import { useState } from "react"; // Importe o useState
+import { useState } from "react"; // Importe o useState
 
 function Mine() {
+
+  const [inputValue, setInputValue] = useState(''); // Estado para armazenar o valor do input
+
+  const handleRegister = () => {
+    // Lógica para registrar o valor, por exemplo, exibindo-o no console
+    console.log('Valor registrado:', inputValue);
+  };
 
   // // Estado para controlar o valor da barra de progresso
   // const [progress, setProgress] = useState(0);
@@ -33,12 +40,24 @@ function Mine() {
         <div className="plant-container">
           <Plant type={"Cenoura"} value={5} quantity={10} image="https://i.imgur.com/TDxiSZE.jpg" />
           <Plant type={"Batata"} value={8} quantity={15} image="https://i.imgur.com/bWT8w9c.jpg" />
-          <Plant type={"Trigo"} value={6} quantity={12} image="https://i.imgur.com/Dm8wkHi.jpg"  />
+          <Plant type={"Trigo"} value={6} quantity={12} image="https://i.imgur.com/Dm8wkHi.jpg" />
+          <Plant type={"Beterraba"} value={2} quantity={2} image="https://i.imgur.com/GvljP5e.jpg" />
+          <Plant type={"Melancia"} value={3} quantity={20} image="https://i.imgur.com/NWrAQPW.jpg"  />
         </div>
 
         <div className="handle-game">
           <p>Progresso da colheita:</p>
           <progress  max={100}></progress> {/* Barra de progresso */}
+
+          <input
+            type="text"
+            placeholder="Insira um valor"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)} // Atualiza o estado com o valor do input
+          />
+
+          <button onClick={handleRegister}>Registrar Valor</button>
+
 
           <div className="button-container">
             <button> tipo de planta</button>
@@ -50,7 +69,7 @@ function Mine() {
 
             <p> Valor atual: ?</p>
 
-            <p> Ainda cabe mais plantas na sua bolsa</p>
+            <p> Ainda cabe mais plantas em sua plantação</p>
 
             <button> Reiniciar </button>
           </div>
